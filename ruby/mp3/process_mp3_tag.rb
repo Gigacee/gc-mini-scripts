902d21ad7eb891d9f44e3be73e2b0388c21dcc7e
Gigacee <t47833@gmail.com>
2019-06-19 23:33:11 +0900

    mp3 ファイルのタグを扱うテストスクリプトを作成

---
+++ ruby/mp3/process_mp3_tag.rb
@@ -0,0 +1,23 @@
+################################################################################
+# mp3 ファイルのタグを扱うテストです。
+################################################################################
+
+require "mp3info"
+
+# 作業ディレクトリのパス
+WORKING_DIRECTORY_PATH = __dir__.encode("UTF-8").freeze
+
+# 作業ディレクトリと同階層にあるすべての mp3 ファイルに対して処理を行う
+Dir.glob("#{WORKING_DIRECTORY_PATH}/*.mp3").each do |f|
+  Mp3Info.open(f) do |mp3|
+    puts "#{f}"
+
+    # トラック番号を設定
+    # mp3.tag2.TRCK = File.basename(f, ".*").gsub(/[^0-9 _\-]*/, "").split(/[ _\-]/).last
+
+    # タグ情報を出力
+    p mp3.tag2
+  end
+
+  puts
+end
